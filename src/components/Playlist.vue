bdda83b8684a060fa4e706e0f4b2658a6189b277
Laís Tomaz <laisotomaz@gmail.com>
2018-05-03 12:42:16 -0300

    Add drag and drop functionality to Playlist

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -3,8 +3,17 @@
 		<h2>Your playlist</h2>
 
 		<ul class="playlist__list" v-if="playlist.length > 0">
-			<li class="track" v-for="track in playlist" :key="track.id">
-				<img class="track__thumb":src="track.album.images[2].url" alt="Album cover">
+			<li
+				class="track"
+				v-for="(track, i) in playlist"
+				:key="track.id"
+				draggable
+				:id="`track-${i}`"
+				@dragover="handleDragOver"
+				@dragstart="handleDragStart"
+				@drop="handleDrop"
+			>
+				<img class="track__thumb" :src="track.album.images[2].url" alt="Album cover">
 				<p class="track__name">{{ track.name }}</p>
 				<button @click="removeTrack(track)">Remove from playlist</button>
 				<button @click="playTrack(track)">Play song</button>
@@ -32,8 +41,49 @@ export default {
 		},
 		playTrack(track) {
 			this.$store.dispatch('UPDATE_CURRENT_TRACK', track);
+		},
+		handleDragStart(e) {
+			e.dataTransfer.effectAllowed = 'move';
+			e.dataTransfer.setData('text/html', e.target.id);
+		},
+		handleDragOver(e) {
+			if (e.preventDefault) {
+				e.preventDefault();
+			}
+
+			e.dataTransfer.dropEffect = 'move'; 
+
+			return false;
+		},
+		handleDrop(e) {
+			if (e.stopPropagation) {
+				e.stopPropagation();
+			}
+
+			const enterId = e.dataTransfer.getData('text/html');
+			const leaveId = e.target.id;
+
+			console.log(enterId, leaveId);
+
+			if (leaveId) {
+				this.swapTracks(enterId.replace('track-', ''), leaveId.replace('track-', ''));
+			}
+
+			return false;
+		},
+		swapTracks(start, end) {
+			const movedItem = this.playlist.find((item, index) => index == start);
+			const remainingItems = this.playlist.filter((item, index) => index != start);
+		
+			const reorderedItems = [
+					...remainingItems.slice(0, end),
+					movedItem,
+					...remainingItems.slice(end),
+			];
+
+			this.$store.dispatch('SWAP_PLAYLIST', reorderedItems);
 		}
-	}
+	},
 }
 </script>
 
