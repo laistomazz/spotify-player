66995e172a0c4a8b039ac7c4c994a78d63cc2e90
Laís Tomaz <laisotomaz@gmail.com>
2018-05-03 11:21:20 -0300

    Add play next button

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,12 +1,20 @@
 <template>
   <section class="player">
 		<h2>Player</h2>
-		<p v-if="currentTrack.name">{{ status === 'playing' ? 'Playing now:' : ''}} {{ currentTrack.name }}</p>
-    <button
+		<p v-if="currentTrack.name">
+			{{ status === 'play' ? 'Playing now:' : ''}} {{ currentTrack.name }}</p>
+    <p v-else>Choose a song to play</p>
+		<button
 			@click="toggleTrack"
 			v-if="status !== ''"
 		>
 			{{ this.status === 'play' ? 'Pause' : 'Play' }}
+		</button>
+		<button
+			@click="playNext"
+			v-if="hasNext > -1"
+		>
+			Next
 		</button>
   </section>
 </template>
@@ -31,6 +39,14 @@ export default {
 		},
 		token() {
 			return this.$store.state.token;
+		},
+		hasNext() {
+			const current = this.playlist.findIndex(item => item.id === this.currentTrack.id);
+			if (current > -1 && current < this.playlist.length - 1) {
+				return current + 1;
+			} else {
+				return -1;
+			}
 		}
 	},
 	mounted() {
@@ -94,8 +110,20 @@ export default {
 			player.pause().then(() => {
 				this.status = 'pause';
 			});
+		},
+		playNext() {
+			this.$store.dispatch('UPDATE_CURRENT_TRACK', this.playlist[this.hasNext]);
 		}
 	}
 }
 </script>
+
+<style>
+.player {
+	padding: 20px 0;
+  border-bottom: 1px solid #c2c2c2;
+  margin-bottom: 20px;
+}
+</style>
+
 
